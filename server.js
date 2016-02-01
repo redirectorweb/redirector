e44d77ff5dab2a0d4d2b935618ced917a021ad74
usuario <nomail>
2016-02-01 15:49:24 -0500

    Limpiar código

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 const Http = require('http');
 const Https = require('https');
 const Url = require('url');
-//const Browser = require('zombie');
 
 var puerto = process.env.PORT || 8080;
 
@@ -21,18 +20,6 @@ function onRequest(client_req, client_res)
 			protocolo = Https;
 		else
 			protocolo = Http;
-
-		// const browser = new Browser();
-
-		// browser.localhost('example.com', 5000);
-
-		// browser.pipeline.addHandler(function(browser, request, response) {
-			// console.log('Response body: ' + response.body);
-		// });
-
-		// browser.visit('/path', function() {
-			// console.log(browser.location.href);
-		// });
 
 		var proxy = protocolo.get(urlSolicitada, function (server_response)
 		{
